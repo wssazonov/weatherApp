24f844aae74ffdebd42365783f9b96e496e765ae
wssazonov <ws.sazonov@gmail.com>
2019-02-14 23:36:05 +0300

    create info

---
+++ src/components/info.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+
+class Info extends Component {
+  render() {
+    return (
+      <div >
+        <h2>
+            Погодное приложение
+        </h2>
+        <p>Узнайте погоду в Вашем городе!</p>
+      </div>
+    );
+  }
+}
+
+export default Info;
