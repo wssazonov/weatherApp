b0684f61154ad4e09e67b1ce22e7a140d6ab7ae2
wssazonov <ws.sazonov@gmail.com>
2019-02-15 00:11:49 +0300

    add apikey

--- src/App.js
+++ src/App.js
@@ -8,6 +8,14 @@ import Weather from './components/weather';
 const API_KEY = 'b46626b1c1a5f40685099d41b1e4535f';
 
 class App extends Component {
+
+  gettingWeather = async () => {
+    const api_url = await 
+    fetch(`http://api.openweathermap.org/data/2.5/weather?q=Olenegorsk,ru&appid=${API_KEY}&units=metric`);
+    const data = await api_url.json();
+    console.log(data);
+  }
+
   render() {
     return (
       <div >
